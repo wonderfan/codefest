c90c5f37d907e78ae93feb5f4dbeba329c7501d0
阿樊提 <fanjiahe2000@163.com>
2017-04-09 14:33:11 +0800

    Update array_usage.sh

--- Bash/array_usage.sh
+++ Bash/array_usage.sh
@@ -1,2 +1,18 @@
-#!/user/bin/env bash
+#!/usr/bin/env bash
 
+# The target of codes: use the array to hold the data 
+#                      and use for loop to output the content
+
+# first step: declare the array variable
+
+declare -a languages
+
+# second step: initialize the array
+
+languages=('bash' 'python' 'go') # use the whitespace as the separator
+
+# for loop
+
+for item in ${languages[@]};do
+    echo ">>>The language is " + $item
+done
