f908beb4b058931426f2f5af89f8c0d19837a2b4
阿樊提 <fanjiahe2000@163.com>
2017-04-10 08:34:01 +0800

    Create main.c

---
+++ C/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// define another function
+
+int sum(int a, int b){
+	return a+b;
+}
+
+int main() {
+    printf("Hello, World!");
+    printf("The sum is %d",sum(1,2));
+    return 0;
+}
+    
+
+
